6a825a9f72825567063ebc2761bc3057f90a52ce
abc <rickykei@yahoo.com.hk>
2022-12-04 10:30:12 +0800

    20221003
    20221003

--- src/http-common2.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { servicePath2 } from 'constants/defaultValues';
- 
-export default axios.create({
-  baseURL: servicePath2,
-  headers: {
-    "Content-type": "multipart/form-data"
-  }
-});
