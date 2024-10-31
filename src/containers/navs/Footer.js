d6f4539558717b79ce435ad95c233b98d10f0f1b
abc <rickykei@yahoo.com.hk>
2024-10-31 13:49:08 +0800

    20241031

--- src/containers/navs/Footer.js
+++ src/containers/navs/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+ 
 import { Row } from 'reactstrap';
 import { Colxx } from 'components/common/CustomBootstrap';
 
@@ -12,25 +12,7 @@ const Footer = () => {
             <Colxx xxs="12" sm="6">
               <p className="mb-0 text-muted">NFC Touch 2024 V4.1</p>
             </Colxx>
-            <Colxx className="col-sm-6 d-none d-sm-block">
-              <ul className="breadcrumb pt-0 pr-0 float-right">
-                <li className="breadcrumb-item mb-0">
-                  <NavLink className="btn-link" to="#" location={{}}>
-                    Review
-                  </NavLink>
-                </li>
-                <li className="breadcrumb-item mb-0">
-                  <NavLink className="btn-link" to="#" location={{}}>
-                    Purchase
-                  </NavLink>
-                </li>
-                <li className="breadcrumb-item mb-0">
-                  <NavLink className="btn-link" to="#" location={{}}>
-                    Docs
-                  </NavLink>
-                </li>
-              </ul>
-            </Colxx>
+            
           </Row>
         </div>
       </div>
